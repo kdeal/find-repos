93d9a63182c9dac7db682f5b11aa782fc6b964bb
Kyle Deal <kdeal@kyledeal.com>
2017-07-11 23:37:56 -0700

    Move checking dir into it's own function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
 use std::path::Path;
 use std::fs;
+use std::io;
 use std::env;
+
+
+fn check_dir(dir: String, repo_root: &str) -> io::Result<Vec<String>> {
+    let mut dir_paths: Vec<String> = vec![];
+    for path in fs::read_dir(dir)? {
+        if path.is_err() {
+            continue;
+        }
+
+        let repo_path = path.unwrap();
+        if repo_path.file_name().eq(".git") {
+            let repo_path_path = repo_path.path();
+            let parent_path = repo_path_path.parent().unwrap();
+            let striped: &Path = parent_path.strip_prefix(repo_root).unwrap();
+            println!("{}", striped.to_str().unwrap());
+            return Ok(vec![]);
+        }
+
+        let file_type = repo_path.file_type().unwrap();
+        if file_type.is_dir() {
+            dir_paths.push(repo_path.path().into_os_string().into_string().unwrap());
+        }
+    }
+    Ok(dir_paths)
+}
 
 fn main() {
     let repo_root = match env::args().nth(1) {
@@ -10,33 +36,11 @@ fn main() {
     let mut dirs_to_read: Vec<String> = vec![repo_root.clone()];
     let repo_root = repo_root.as_str();
 
-    'dirs: while !dirs_to_read.is_empty() {
+    while !dirs_to_read.is_empty() {
         let path = dirs_to_read.pop().unwrap();
-        let paths = fs::read_dir(path);
-        if !paths.is_ok() {
-            continue;
+        let dir_paths = check_dir(path, repo_root);
+        if dir_paths.is_ok() {
+            dirs_to_read.append(&mut dir_paths.unwrap());
         }
-
-        let mut dir_paths: Vec<String> = vec![];
-        for path in paths.unwrap() {
-            if !path.is_ok() {
-                continue;
-            }
-
-            let repo_path = path.unwrap();
-            if repo_path.file_name().eq(".git") {
-                let repo_path_path = repo_path.path();
-                let parent_path = repo_path_path.parent().unwrap();
-                let striped: &Path = parent_path.strip_prefix(repo_root).unwrap();
-                println!("{}", striped.to_str().unwrap());
-                continue 'dirs;
-            }
-
-            let file_type = repo_path.file_type().unwrap();
-            if file_type.is_dir() {
-                dir_paths.push(repo_path.path().into_os_string().into_string().unwrap());
-            }
-        }
-        dirs_to_read.append(&mut dir_paths);
     }
 }
