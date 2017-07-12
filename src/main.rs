f77ba4fd7d6c7a6c0beb558cfcb2b4f441c8a896
Kyle Deal <kdeal@kyledeal.com>
2017-07-11 22:45:55 -0700

    Small improvements
    - Get rid of git_dir since OsString.eq can take string reference
    - Append new dirs to be read only if no .git dir is found

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::path::Path;
 use std::fs;
 use std::env;
-use std::ffi::OsString;
 
 fn main() {
-    let git_dir = OsString::from(".git");
     let repo_root = match env::args().nth(1) {
         Some(path) => path,
         None => format!("{}/repos", env::home_dir().unwrap().to_str().unwrap()),
@@ -12,31 +10,33 @@ fn main() {
     let mut dirs_to_read: Vec<String> = vec![repo_root.clone()];
     let repo_root = repo_root.as_str();
 
-    while !dirs_to_read.is_empty() {
+    'dirs: while !dirs_to_read.is_empty() {
         let path = dirs_to_read.pop().unwrap();
         let paths = fs::read_dir(path);
         if !paths.is_ok() {
             continue;
         }
 
+        let mut dir_paths: Vec<String> = vec![];
         for path in paths.unwrap() {
             if !path.is_ok() {
                 continue;
             }
 
             let repo_path = path.unwrap();
-            if repo_path.file_name() == git_dir {
+            if repo_path.file_name().eq(".git") {
                 let repo_path_path = repo_path.path();
                 let parent_path = repo_path_path.parent().unwrap();
                 let striped: &Path = parent_path.strip_prefix(repo_root).unwrap();
                 println!("{}", striped.to_str().unwrap());
-                break;
+                continue 'dirs;
             }
 
             let file_type = repo_path.file_type().unwrap();
             if file_type.is_dir() {
-                dirs_to_read.push(repo_path.path().into_os_string().into_string().unwrap());
+                dir_paths.push(repo_path.path().into_os_string().into_string().unwrap());
             }
         }
+        dirs_to_read.append(&mut dir_paths);
     }
 }
